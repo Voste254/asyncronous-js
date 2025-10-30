12978f9f3b764e8e41243f338f3df1e42a4f8f7c
Voste254 <onyangosteveok@gmail.com>
2025-10-30 18:54:39 +0300

    modifications

--- dist/app.js
+++ dist/app.js
@@ -15,12 +15,17 @@ function greet(name, age) {
     console.log(`hey ${name}, you are ${age}!`);
 }
 function getArea(shape) {
+    const opening=`the area of your ${shape.kind} is `
     switch (shape.kind) {
         case "circle":
-            return Math.PI * Math.pow(shape.radius, 2);
+           return opening + Math.PI * Math.pow(shape.radius, 2)
             break;
         case "square":
-            return Math.pow(shape.side, 2);
+            return opening + Math.pow(shape.side, 2)
+            break;
+        case "rectangle":
+            return opening + shape.length * shape.width
     }
 }
-console.log(getArea({ kind: "square", side: 5 }));
+console.log(getArea({ kind: "rectangle", length: 14, width:5 }));
+//console.log(getArea({kind: "circle" ,radius:14}))
---
+++ dist/dom.js
@@ -0,0 +1,14 @@
+const ul = document.querySelector("ul")
+ console.log(ul)
+// console.log(title.innerText)
+// console.log(title.textContent)
+// console.log(title.innerHTML)
+const li = document.createElement('li')
+ul.append(li)
+li.innerText='Voste'
+// li.setAttribute('class','list-items')
+li.classList.add('list-items')
+// title.style.color = 'red'
+// const container = document.querySelector('div')
+// console.log(container) 
+ 
