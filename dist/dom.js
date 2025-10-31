d46b4e8cb6e5548b6fb251c7514b8073fb4f9491
Voste254 <onyangosteveok@gmail.com>
2025-10-31 18:59:12 +0300

    Modifications

--- dist/dom.js
+++ dist/dom.js
@@ -1,5 +1,5 @@
 const ul = document.querySelector("ul")
- console.log(ul)
+//  console.log(ul)
 // console.log(title.innerText)
 // console.log(title.textContent)
 // console.log(title.innerHTML)
@@ -11,4 +11,8 @@ li.classList.add('list-items')
 // title.style.color = 'red'
 // const container = document.querySelector('div')
 // console.log(container) 
- 
+const list = document.getElementsByClassName("list-items")
+console.log(list.textContext)
+ for (i=0;i<list.length;i++){
+    list.style.color='red'
+ }
